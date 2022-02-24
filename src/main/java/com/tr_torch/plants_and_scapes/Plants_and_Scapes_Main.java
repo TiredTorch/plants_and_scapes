a1f03924b15bcb12e5deacb964c9f00f4b8b0f70
Tr.Torch <vitochke337@gmail.com>
2022-02-24 12:48:59 +0200

    Initial commit

---
+++ src/main/java/com/tr_torch/plants_and_scapes/Plants_and_Scapes_Main.java
@@ -0,0 +1,29 @@
+package com.tr_torch.plants_and_scapes;
+
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+@Mod(Plants_and_Scapes_Main.MOD_ID)
+public class Plants_and_Scapes_Main
+{
+    public static final String MOD_ID = "plants_and_scapes";
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public Plants_and_Scapes_Main() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event)
+    {
+        LOGGER.info("HELLO FROM PREINIT");
+        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
