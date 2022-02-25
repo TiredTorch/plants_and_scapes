efa40e32c345814cad5288652c56ffd333d2bf3d
Tr.Torch <vitochke337@gmail.com>
2022-02-25 16:23:21 +0200

    Created 2 items: light medicine and medium medicine

--- src/main/java/com/tr_torch/plants_and_scapes/Plants_and_Scapes_Main.java
+++ src/main/java/com/tr_torch/plants_and_scapes/Plants_and_Scapes_Main.java
@@ -1,7 +1,9 @@
 package com.tr_torch.plants_and_scapes;
 
+import com.tr_torch.plants_and_scapes.item.ModItems;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -16,7 +18,11 @@ public class Plants_and_Scapes_Main
     private static final Logger LOGGER = LogManager.getLogger();
 
     public Plants_and_Scapes_Main() {
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(eventBus);
+
+        eventBus.addListener(this::setup);
 
         MinecraftForge.EVENT_BUS.register(this);
     }
---
+++ src/main/java/com/tr_torch/plants_and_scapes/item/ModItems.java
@@ -0,0 +1,26 @@
+package com.tr_torch.plants_and_scapes.item;
+
+import com.tr_torch.plants_and_scapes.Plants_and_Scapes_Main;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, Plants_and_Scapes_Main.MOD_ID);
+
+    public static final RegistryObject<Item> LIGHT_MEDICINE = ITEMS.register("light_medicine",
+            () -> new Item(
+                    new Item.Properties().tab(CreativeModeTab.TAB_BREWING)));
+
+    public static final RegistryObject<Item> MEDIUM_MEDICINE = ITEMS.register("medium_medicine",
+            () -> new Item(
+                    new Item.Properties().tab(CreativeModeTab.TAB_BREWING)));
+
+    public static void register(IEventBus eventBus){
+        ITEMS.register(eventBus);
+    }
+}
